b2f620fba4bddfe81661ae9787f59ef5b570a3e6
Tapani Utriainen <tapani@technexion.com>
2018-05-31 16:40:26 +0800

    input: touchscreen: edt-ft5x06: enable DT controlled direct mode for newer Androids

--- drivers/input/touchscreen/edt-ft5x06.c
+++ drivers/input/touchscreen/edt-ft5x06.c
@@ -1000,6 +1000,7 @@ static void edt_ft5x06_ts_get_defaults(struct device *dev,
 				       struct edt_ft5x06_ts_data *tsdata)
 {
 	struct edt_reg_addr *reg_addr = &tsdata->reg_addr;
+	struct input_dev *input;
 	u32 val;
 	int error;
 
@@ -1038,6 +1039,15 @@ static void edt_ft5x06_ts_get_defaults(struct device *dev,
 						  reg_addr->reg_offset_y, val);
 		tsdata->offset_y = val;
 	}
+
+	error = device_property_read_u32(dev, "direct", &val);
+	if (!error && val) {
+		input = tsdata->input;
+		__set_bit(EV_KEY, input->evbit);
+		__set_bit(EV_ABS, input->evbit);
+		__set_bit(BTN_TOUCH, input->keybit);
+		__set_bit(INPUT_PROP_DIRECT, input->propbit);
+        }
 }
 
 static void edt_ft5x06_ts_get_parameters(struct edt_ft5x06_ts_data *tsdata)
