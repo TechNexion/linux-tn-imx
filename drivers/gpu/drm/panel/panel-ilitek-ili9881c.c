257318a51790da8e409325d7a36724db98c2351e
Richard Hu <richard.hu@technexion.com>
2019-05-07 00:07:18 -0700

    drm/panel: ILI9881c: enable 5 inch ili9881C for PICO-IMX8M
    The patch is based on the work done by W.P. Liu from NXP

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -22,6 +22,15 @@
 
 #include <video/mipi_display.h>
 
+
+struct ili9881c {
+	struct drm_panel	panel;
+	struct mipi_dsi_device	*dsi;
+
+	struct regulator	*power;
+	struct gpio_desc	*reset;
+};
+
 enum ili9881c_op {
 	ILI9881C_SWITCH_PAGE,
 	ILI9881C_COMMAND,
@@ -1305,6 +1314,8 @@ static int ili9881c_enable(struct drm_panel *panel)
 	unsigned int i;
 	int ret;
 
+	ctx->dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+
 	for (i = 0; i < ARRAY_SIZE(ili9881c_init); i++) {
 		const struct ili9881c_instr *instr = &ili9881c_init[i];
 
@@ -1358,14 +1369,26 @@ static int ili9881c_unprepare(struct drm_panel *panel)
 	return 0;
 }
 
-static const struct drm_display_mode lhr050h41_default_mode = {
-	.clock		= 62000,
+static const struct drm_display_mode bananapi_default_mode = {
+	.clock		= 108000,
+	.vrefresh	= 60,
+	.hdisplay	= 720,
+	.hsync_start	= 720 + 34,
+	.hsync_end	= 720 + 34 + 100,
+	.htotal	= 720 + 34 + 100 + 100,
+	.vdisplay	= 1280,
+	.vsync_start	= 1280 + 2,
+	.vsync_end	= 1280 + 2 + 30,
+	.vtotal	= 1280 + 2 + 30 + 20,
+};
 
+static const struct drm_display_mode origin_mode = {
+	.clock		= 62000,
+	.vrefresh	= 60,
 	.hdisplay	= 720,
 	.hsync_start	= 720 + 10,
 	.hsync_end	= 720 + 10 + 20,
 	.htotal		= 720 + 10 + 20 + 30,
-
 	.vdisplay	= 1280,
 	.vsync_start	= 1280 + 10,
 	.vsync_end	= 1280 + 10 + 10,
@@ -1465,6 +1488,7 @@ static int ili9881c_get_modes(struct drm_panel *panel,
 {
 	struct ili9881c *ctx = panel_to_ili9881c(panel);
 	struct drm_display_mode *mode;
+	const struct drm_display_mode *display_mode;
 	u32 bus_format = MEDIA_BUS_FMT_RGB888_1X24;
 	int ret;
 
@@ -1558,7 +1582,7 @@ static int ili9881c_dsi_probe(struct mipi_dsi_device *dsi)
 
 	drm_panel_add(&ctx->panel);
 
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_VIDEO | ctx->desc->mode_flags;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_VIDEO | MIPI_DSI_CLOCK_NON_CONTINUOUS/*| ctx->desc->mode_flags*/;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->lanes = 4;
 
