0cd166702fd93f8680b4ba6cd3f3cda052ca4ee8
Richard Hu <richard.hu@technexion.com>
2019-05-21 01:22:07 -0700

    ARM: drm: panel: add additional set of timing parameters for AUO G080UAN01 panel especially for i.mx8MQ
    The MIPI phy driver for i.mx8m is only allowed to accept specifc clocks:
    In drivers/gpu/drm/bridge/nwl-dsi.c :
    -------------------------------------------
    static int valid_clocks[] = {
    162000,
    148500,
    135000,
    132000,
    108000,
    74250,
    49500,
    31500,
    };
    -------------------------------------------

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -2395,6 +2395,34 @@ static const struct panel_desc_dsi auo_g080uan01 = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode auo_g080uan01_imx8m_mode = {
+	.clock = 148500,
+	.hdisplay = 1200,
+	.hsync_start = 1200 + 80,
+	.hsync_end = 1200 + 80 + 1,
+	.htotal = 1200 + 80 + 1 + 60,
+	.vdisplay = 1920,
+	.vsync_start = 1920 + 35,
+	.vsync_end = 1920 + 35 + 1,
+	.vtotal = 1920 + 35 + 1 + 25,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc_dsi auo_g080uan01_imx8m = {
+	.desc = {
+		.modes = &auo_g080uan01_imx8m_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 184,
+			.height = 114,
+		},
+		.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
+	},
+	.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
 
 static struct drm_display_mode boe_tv080wum_nl0_mode = {
 	.clock = 160000,
@@ -2548,6 +2576,9 @@ static const struct of_device_id dsi_of_match[] = {
 	}, {
 		.compatible = "auo,g080uan01",
 		.data = &auo_g080uan01
+	}, {
+		.compatible = "auo,g080uan01_imx8m",
+		.data = &auo_g080uan01_imx8m
 	}, {
 		.compatible = "auo,g101uan02",
 		.data = &auo_g101uan02
