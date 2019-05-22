TechNexion-customization/linux-imx-tn

drm/panel: simple: add support for AUO G101UAN02 panel
This patch adds support for AUO 10.1 inch G101UAN02
WUXGA(1920x1200) TFT LCD panel with MIPI DSI interface.

drivers/gpu/drm/panel/panel-simple.c
@@ -2253,6 +2253,37 @@ static const struct panel_desc_dsi auo_b080uan01 = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode auo_g101uan02_mode = {
+	.clock = 149900,
+	.hdisplay = 1920,
+	.hsync_start = 1920 + 60,
+	.hsync_end = 1920 + 60 + 18,
+	.htotal = 1920 + 60 + 18 + 60,
+	.vdisplay = 1200,
+	.vsync_start = 1200 + 5,
+	.vsync_end = 1200 + 5 + 2,
+	.vtotal = 1200 + 5 + 2 + 5,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc_dsi auo_g101uan02 = {
+	.desc = {
+		.modes = &auo_g101uan02_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 217,
+			.height = 136,
+		},
+		.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
+	},
+	//.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE,
+	.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
+
+
 static const struct drm_display_mode boe_tv080wum_nl0_mode = {
 	.clock = 160000,
 	.hdisplay = 1200,
@@ -2372,6 +2403,9 @@ static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "auo,b080uan01",
 		.data = &auo_b080uan01
+	}, {
+		.compatible = "auo,g101uan02",
+		.data = &auo_g101uan02
 	}, {
 		.compatible = "boe,tv080wum-nl0",
 		.data = &boe_tv080wum_nl0

TechNexion/linux-tn-imx

drivers/gpu/drm/panel/panel-simple.c
@@ -2308,6 +2308,36 @@ static const struct panel_desc_dsi auo_b080uan01 = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode auo_g101uan02_mode = {
+	.clock = 149900,
+	.hdisplay = 1920,
+	.hsync_start = 1920 + 60,
+	.hsync_end = 1920 + 60 + 18,
+	.htotal = 1920 + 60 + 18 + 60,
+	.vdisplay = 1200,
+	.vsync_start = 1200 + 5,
+	.vsync_end = 1200 + 5 + 2,
+	.vtotal = 1200 + 5 + 2 + 5,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc_dsi auo_g101uan02 = {
+	.desc = {
+		.modes = &auo_g101uan02_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 217,
+			.height = 136,
+		},
+		.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
+	},
+	.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
+
+
 static struct drm_display_mode boe_tv080wum_nl0_mode = {
 	.clock = 160000,
 	.hdisplay = 1200,
@@ -2457,6 +2487,9 @@ static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "auo,b080uan01",
 		.data = &auo_b080uan01
+	}, {
+		.compatible = "auo,g101uan02",
+		.data = &auo_g101uan02
 	}, {
 		.compatible = "boe,tv080wum-nl0",
 		.data = &boe_tv080wum_nl0