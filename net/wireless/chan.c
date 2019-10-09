50c5133593bd623cd7b2aaee05867dbb26f34cce
Sherry Sun <sherry.sun@nxp.com>
2019-10-09 09:54:00 -0400

    MLK-22746-3 wireless: Changes for wireless and cfg80211 support
    Pulling the following commits and some general changes from custom
    v3.10 kernel for supporting qcacld2.0 on kernel v4.9.11.
    1. cfg80211: Using new wiphy flag WIPHY_FLAG_DFS_OFFLOAD
    When flag WIPHY_FLAG_DFS_OFFLOAD is defined, the driver would handle
    all the DFS related operations. Therefore the kernel needs to ignore
    the DFS state that it uses to block the userspace calls to the driver
    through cfg80211 APIs. Also it should treat the userspace calls to
    start radar detection as a no-op.
    
    Please note that changes in util.c is not picked up explicitly.
    Kernel v4.9.11 uses wrapper cfg80211_get_chans_dfs_required which takes
    care of this change.
    
    Change-Id: I9dd2076945581ca67e54dfc96dd3dbc526c6f0a2
    IRs-Fixed: 202686
    
    2. New db.txt from git/sforshee/wireless-regdb.git
    CONFIG_CFG80211_INTERNAL_REGDB is enabled in build. This causes
    kernel warn messages as db.txt is empty. A new db.txt is added
    from:
    git://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git
    
    IRs-Fixed: 202686
    
    3. Picked up the declaration and definition of the function
    cfg80211_is_gratuitous_arp_unsolicited_na
    
    Change-Id: I1e4083a2327c121073226aa6b75bb6b5b97cec00
    CRs-fixed: 1079453
    
    Signed-off-by: Nakul Kachhwaha <nkachh@codeaurora.org>
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    (Vipul: Fixed merge conflicts)
    (TODO: checkpatch warnings)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- net/wireless/chan.c
+++ net/wireless/chan.c
@@ -1336,7 +1336,8 @@ bool _cfg80211_chandef_usable(struct wiphy *wiphy,
 			return false;
 		if (c->flags & permitting_flags)
 			continue;
-		if (c->flags & prohibited_flags)
+		if ((c->flags & prohibited_flags) &&
+			!(wiphy->flags & WIPHY_FLAG_DFS_OFFLOAD))
 			return false;
 	}
 
