cd2f53ade3226b3bcbb3626d010b22bb1cb74e8f
John Weber <john.weber@technexion.com>
2020-01-21 19:59:56 -0800

    drivers:mfd:axonfabric: Separate GPIO driver from core driver
    As a next step to creating an organized MFD driver, we need to separate the
    GPIO functionality from the core driver functions. This is not perfect, but
    is a good first step.

--- include/linux/mfd/axonfabric.h
+++ include/linux/mfd/axonfabric.h
@@ -4,6 +4,83 @@
 
 #include <linux/types.h>
 #include <linux/i2c.h>
+#include <linux/gpio.h>
+#include <linux/gpio/driver.h>
+
+#define AXONF_ADDR_MAGIC   0x0000
+#define AXONF_ADDR_VERSION 0x0004
+#define AXONF_ADDR_CONFIG  0x0008
+#define AXONF_ADDR_CTRLREG 0x0010
+#define AXONF_ADDR_RGBLED  0x0020
+#define AXONF_ADDR_IOBLOCK 0x0100
+
+#define AXONF_SIZE_MAGIC   4
+#define AXONF_SIZE_VERSION 3
+#define AXONF_SIZE_CONFIG  1
+#define AXONF_SIZE_CTRLREG 2
+#define AXONF_SIZE_RGBLED  3
+#define AXONF_SIZE_IOBLOCK 256
+
+/* For Axon Fabric Firmware version 0.2.0 and later */
+
+#define AXONF_IOB_OFFSET_EN			0
+#define AXONF_IOB_OFFSET_PP			1
+#define AXONF_IOB_OFFSET_DIR		2
+#define AXONF_IOB_OFFSET_SEL		3
+#define AXONF_IOB_OFFSET_ODR		5
+#define AXONF_IOB_OFFSET_IDR		6
+#define AXONF_IOB_OFFSET_INT_IDR	7
+
+#define AXONF_REGMAP_NAME	"axonfabric_regmap"
+
+#define AXONF_IOB_MASK_ENABLE		(0x01 << AXONF_IOB_OFFSET_EN)
+#define AXONF_IOB_MASK_PP			(0x01 << AXONF_IOB_OFFSET_PP)
+#define AXONF_IOB_MASK_DIR			(0x01 << AXONF_IOB_OFFSET_DIR)
+#define AXONF_IOB_MASK_SEL			(0x03 << AXONF_IOB_OFFSET_SEL)
+#define AXONF_IOB_MASK_ODR			(0x01 << AXONF_IOB_OFFSET_ODR)
+#define AXONF_IOB_MASK_IDR			(0x01 << AXONF_IOB_OFFSET_IDR)
+#define AXONF_IOB_MASK_INT_IDR		(0x01 << AXONF_IOB_OFFSET_INT_IDR)
+
+#define AXONF_IOB_REG_CTRLSTATUS	0x00
+#define AXONF_IOB_REG_IRQ			0x08
+
+
+/* IO Block definitions */
+#define AXONF_IOB_SEL_PASSTHROUGH   0x0
+#define AXONF_IOB_SEL_GPIO          0x3
+#define AXONF_IOB_SEL_ALT0          0x1
+#define AXONF_IOB_SEL_ALT1          0x2
+
+#define AXONF_CTRLREG_GIO_ENABLE				0
+#define AXONF_CTRLREG_SHARED_SYSCONFIG_DISABLE	1
+
+#define AXONF_NGPIOS					104
+#define AXONF_NBANKS					13
+
+#define MAX_BANK 		13
+#define BANK_SZ 		8
+
+/*
+ * Driver data and types
+ */
+#define AXONF_GPIO_MASK					0x000FF
+#define AXONF_INT_MASK					0x00100
+#define AXONF_TYPE_MASK					0xFF000
+#define AXONF_SOC_MASK 					0x0F000
+#define AXONF_FLVL_MASK					0xF0000
+#define AXONF_FLVL_1					0x10000
+#define AXONF_FLVL_2					0x20000
+#define AXONF_FLVL_3					0x30000
+
+#define AXONF_INT						0x00100
+#define AXONF_IMX6						0x01000
+#define AXONF_IMX8MM					0x02000
+
+#define AXONF_CHIP_TYPE(x)	((x) & AXONF_TYPE_MASK)
+#define AXONF_SOC_TYPE(x)	((x) & AXONF_SOC_MASK)
+
+// Enable DEBUGFS
+#define CONFIG_AXONF_DEBUGFS
 
 /* platform data for the Axon Fabric driver */
 
@@ -28,18 +105,47 @@ struct axonf_platform_data {
 	const char	*const *names;
 };
 
-#define AXONF_ADDR_MAGIC   0x0000
-#define AXONF_ADDR_VERSION 0x0004
-#define AXONF_ADDR_CONFIG  0x0008
-#define AXONF_ADDR_CTRLREG 0x0010
-#define AXONF_ADDR_RGBLED  0x0020
-#define AXONF_ADDR_IOBLOCK 0x0100
+struct axonf_reg_config {
+	int ctrl_status;
+	int irq;
+};
 
-#define AXONF_SIZE_MAGIC   4
-#define AXONF_SIZE_VERSION 3
-#define AXONF_SIZE_CONFIG  1
-#define AXONF_SIZE_CTRLREG 2
-#define AXONF_SIZE_RGBLED  3
-#define AXONF_SIZE_IOBLOCK 256
+static const struct axonf_reg_config axonf_regs = {
+	.ctrl_status =       AXONF_IOB_REG_CTRLSTATUS,
+	.irq =     			 AXONF_IOB_REG_IRQ
+};
+
+struct axonf_chip {
+	u8 reg_ctrl_status[MAX_BANK*8];
+	u8 reg_irq[MAX_BANK*8];
+	u8 bank_mask[MAX_BANK];
+	u8 allocated[MAX_BANK];
+	u8 fw_version[AXONF_SIZE_VERSION];
+	struct regmap* regmap;
+
+#if 0 // #ifdef CONFIG_GPIO_PCA953X_IRQ
+	struct mutex irq_lock;
+	u8 irq_mask[MAX_BANK];
+	u8 irq_stat[MAX_BANK];
+	u8 irq_trig_raise[MAX_BANK];
+	u8 irq_trig_fall[MAX_BANK];
+#endif
+
+#ifdef CONFIG_AXONF_DEBUGFS
+	struct dentry *debugfs_top_dir;
+#endif
+
+	struct i2c_client *client;
+	unsigned long driver_data;
+	struct regulator *regulator;
+	u8 dir_lock;
+
+	const struct axonf_reg_config *regs;
+
+	u32 statusled_rgb_color;
+	u16 ctrlreg;
+	u8 cfgdata;
+
+};
 
 #endif /* _LINUX_AXONFABRIC_H */
