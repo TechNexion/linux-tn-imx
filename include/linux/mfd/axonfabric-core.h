c58c1a69dba37e5c1b59d3a58b65f6bb936db01b
John Weber <john.weber@technexion.com>
2020-01-28 12:38:55 -0800

    drivers:mfd:axonfabric: Major cleanup
    Since we are using regmap, use regmap_update_bits to perform RMW accesses
    and remove the internal caching of iobank registers as regmap can
    automatically maintain that for us. Remove the axonf_iob_... access
    functions to simplify and consolidate the code.

--- include/linux/mfd/axonfabric-core.h
+++ include/linux/mfd/axonfabric-core.h
@@ -6,12 +6,14 @@
 
 struct axonf_chip;
 
-extern int axonf_iob_read_single(struct axonf_chip *chip, int reg, u8 *val,
-				int off);
-extern int axonf_iob_write_single(struct axonf_chip *chip, int reg, u8 val,
-				int off);
-extern int axonf_iob_read_reg(struct axonf_chip *chip, int reg, u8 *val,
-				int bank);
-extern int axonf_iob_write_reg(struct axonf_chip *chip, int reg, u8 val, int bank);
+extern int axonf_reg_read(struct axonf_chip *chip, unsigned short reg);
+extern int axonf_bulk_read(struct axonf_chip *chip, unsigned short reg,
+		     int count, u8 *buf);
+extern int axonf_reg_write(struct axonf_chip *chip, unsigned short reg,
+		    u8 val);
+extern int axonf_bulk_write(struct axonf_chip *chip, unsigned short reg,
+			int count, u8 *buf);
+extern int axonf_reg_update_bits(struct axonf_chip *chip, unsigned short reg,
+			u8 mask, u8 val);
 
 #endif /* _LINUX_AXONFABRIC_CORE_H */
