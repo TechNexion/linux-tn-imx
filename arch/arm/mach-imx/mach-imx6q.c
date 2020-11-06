8aba52fa5e377b217ab621d8ba96db3f67767d2a
Richard Hu <richard.hu@technexion.com>
2020-11-06 01:23:59 -0800

    ARM: imx: remove hard-code RGMII IO voltageas(ethernet PHY) as 1.8V for iMX6
    The RGMII IO voltage might be different and depend on the HW design() on different boards.
    It would be better to set RGMII IO voltage in DTS to adapt to the HW design on boards.
    
    For example:
      ethphy0: ethernet-phy@0 {
          compatible = "ethernet-phy-ieee802.3-c22";
          reg = <0>;
          at803x,vddio-1p8v;
      };

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -93,10 +93,6 @@ static int ar8031_phy_fixup(struct phy_device *dev)
 {
 	u16 val;
 
-	/* Set RGMII IO voltage to 1.8V */
-	phy_write(dev, 0x1d, 0x1f);
-	phy_write(dev, 0x1e, 0x8);
-
 	/* disable phy AR8031 SmartEEE function. */
 	phy_write(dev, 0xd, 0x3);
 	phy_write(dev, 0xe, 0x805d);
