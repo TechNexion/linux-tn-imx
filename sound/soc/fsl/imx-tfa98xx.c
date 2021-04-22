771786550ad69c6a689cde0b668816a145520772
Wig Cheng <wig.cheng@technexion.com>
2021-04-22 13:37:12 +0800

    sound: imx-tfa98xx: change obtain cpu_dai and codec_dai methods
    Due to cpu_dai and codec_dai members was be dropped from struct
    snd_soc_dai since kernel 5.10, need new way to get cpu_dai and codec_dai
    using asoc_rtd_to_cpu() and asoc_rtd_to_codec().

--- sound/soc/fsl/imx-tfa98xx.c
+++ sound/soc/fsl/imx-tfa98xx.c
@@ -84,7 +84,7 @@ static int imx_tfa98xx_hw_params(struct snd_pcm_substream *substream,
 			 struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
 	int ret = 0;
 
 	/* set cpu DAI configuration */
@@ -94,7 +94,7 @@ static int imx_tfa98xx_hw_params(struct snd_pcm_substream *substream,
 		dev_err(cpu_dai->dev, "failed to set cpu dai fmt\n");
 		return ret;
 	}
-	ret = snd_soc_dai_set_fmt(rtd->codec_dai, SND_SOC_DAIFMT_I2S
+	ret = snd_soc_dai_set_fmt(asoc_rtd_to_codec(rtd, 0), SND_SOC_DAIFMT_I2S
 			| SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS);
 	
 	if (ret) {
