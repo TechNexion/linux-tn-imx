56d43f0014e181864afedd773890dd07257a0dc0
Wig Cheng <wig.cheng@technexion.com>
2021-07-26 14:29:12 +0800

    drivers: usb: cdc-acm: add SERIAL_XR macro to fix compile failed bug

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -1915,6 +1915,7 @@ static const struct usb_device_id acm_ids[] = {
 	},
 #endif
 
+#if IS_ENABLED(CONFIG_USB_SERIAL_XR)
 	{ USB_DEVICE(0x04e2, 0x1400), .driver_info = IGNORE_DEVICE },
 	{ USB_DEVICE(0x04e2, 0x1401), .driver_info = IGNORE_DEVICE },
 	{ USB_DEVICE(0x04e2, 0x1402), .driver_info = IGNORE_DEVICE },
@@ -1926,6 +1927,7 @@ static const struct usb_device_id acm_ids[] = {
 	{ USB_DEVICE(0x04e2, 0x1420), .driver_info = IGNORE_DEVICE },
 	{ USB_DEVICE(0x04e2, 0x1422), .driver_info = IGNORE_DEVICE },
 	{ USB_DEVICE(0x04e2, 0x1424), .driver_info = IGNORE_DEVICE },
+#endif
 
 	/*Samsung phone in firmware update mode */
 	{ USB_DEVICE(0x04e8, 0x685d),
