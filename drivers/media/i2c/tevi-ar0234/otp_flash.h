ed541e5ed8edab7de40d07d90733c4512eaec8c7
Leo Wang <leo.wang@technexion.com>
2021-09-17 15:29:32 +0800

    media: tevi-ar0234: tevi-ar0234 driver implement
    V4l2 sub-device driver implement for TechNexion TEVI-AR0234 sensor
    module

---
+++ drivers/media/i2c/tevi-ar0234/otp_flash.h
@@ -0,0 +1,17 @@
+#ifndef __OTP_FLASH_H__
+#define __OTP_FLASH_H__
+
+#include <linux/i2c.h>
+
+#define BOOT_DATA_START_REG (0x8000)
+#define BOOT_DATA_END_REG (0x9FFF)
+#define BOOT_DATA_WRITE_LEN (BOOT_DATA_END_REG - BOOT_DATA_START_REG + 1)
+
+struct otp_flash;
+struct otp_flash *otp_flash_init(struct device *dev);
+u16 otp_flash_get_checksum(struct otp_flash *instance);
+size_t otp_flash_read(struct otp_flash *instance, u8 *data, int addr, size_t len);
+size_t otp_flash_get_pll_length(struct otp_flash *instance);
+size_t otp_flash_get_pll_section(struct otp_flash*instance, u8 *data);
+
+#endif
