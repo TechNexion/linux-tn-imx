090c9604dbc5a540c54bbf847f866698c9b76a8b
Leo Wang <leo.wang@technexion.com>
2021-10-14 18:18:45 +0800

    media: tevi-ar* sensor: driver build-in default instead of module

--- drivers/media/i2c/tevi-ar0234/otp_flash.h
+++ drivers/media/i2c/tevi-ar0234/otp_flash.h
@@ -8,10 +8,10 @@
 #define BOOT_DATA_WRITE_LEN (BOOT_DATA_END_REG - BOOT_DATA_START_REG + 1)
 
 struct otp_flash;
-struct otp_flash *otp_flash_init(struct device *dev);
-u16 otp_flash_get_checksum(struct otp_flash *instance);
-size_t otp_flash_read(struct otp_flash *instance, u8 *data, int addr, size_t len);
-size_t otp_flash_get_pll_length(struct otp_flash *instance);
-size_t otp_flash_get_pll_section(struct otp_flash*instance, u8 *data);
+struct otp_flash *ar0234_otp_flash_init(struct device *dev);
+u16 ar0234_otp_flash_get_checksum(struct otp_flash *instance);
+size_t ar0234_otp_flash_read(struct otp_flash *instance, u8 *data, int addr, size_t len);
+size_t ar0234_otp_flash_get_pll_length(struct otp_flash *instance);
+size_t ar0234_otp_flash_get_pll_section(struct otp_flash*instance, u8 *data);
 
 #endif
