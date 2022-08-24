19df3aab1ed3b92979ea379d2fc9a86e8d12bb12
Ray Chang <ray.chang@technexion.com>
2022-08-24 16:52:22 +0800

    net: phy: realtek: add dt property to disable PHY-mode EEE
    PHY-mode EEE(Energy Efficient Ethernet) is enabled by default after PHY
    hardware reset, this patch adds realtek,phy-mode-eee-disable property
    for user to disable it.

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -42,6 +42,7 @@
 #define RTL8211E_TX_DELAY			BIT(12)
 #define RTL8211E_RX_DELAY			BIT(11)
 
+#define RTL8211F_PHY_MODE_EEE_EN		BIT(5)
 #define RTL8211F_CLKOUT_EN			BIT(0)
 
 #define RTL8201F_ISR				0x1e
@@ -126,6 +127,10 @@ static int rtl821x_probe(struct phy_device *phydev)
 		if (of_property_read_bool(dev->of_node, "realtek,clkout-disable"))
 			priv->phycr2 &= ~RTL8211F_CLKOUT_EN;
 	}
+
+	priv->phycr2 = ret & RTL8211F_PHY_MODE_EEE_EN;
+	if (of_property_read_bool(dev->of_node, "realtek,phy-mode-eee-disable"))
+		priv->phycr2 &= ~RTL8211F_PHY_MODE_EEE_EN;
 
 	phydev->priv = priv;
 
@@ -419,6 +424,14 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 				ERR_PTR(ret));
 			return ret;
 		}
+	}
+
+	ret = phy_modify_paged(phydev, 0xa43, RTL8211F_PHYCR2,
+			       RTL8211F_PHY_MODE_EEE_EN, priv->phycr2);
+	if (ret < 0) {
+		dev_err(dev, "PHY-mode EEE configuration failed: %pe\n",
+			ERR_PTR(ret));
+		return ret;
 	}
 
 	oldpage = phy_select_page(phydev, RTL8211F_PHYLED_PAGE);
