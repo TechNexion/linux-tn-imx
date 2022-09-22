b2dcd19aefbdb1e96ad0d737be482700dbd4ded8
Dennis Tseng <dennis.tseng@technexion.com>
2022-09-22 01:41:47 -0700

    media: tevi-ap1302: modified different resolution & frame rates for diffrent AR sensor setting.

--- drivers/media/i2c/tevi-ap1302/sensor_tbls.h
+++ drivers/media/i2c/tevi-ap1302/sensor_tbls.h
@@ -13,31 +13,39 @@ static struct resolution ar0144_res_list[] = {
 };
 
 static struct resolution ar0234_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 100},
-	{.width = 1920, .height = 1080, .framerates = 100},
-	{.width = 1920, .height = 1200, .framerates = 100},
+	{.width = 1280, .height = 720, .framerates = 120},
+	{.width = 1920, .height = 1080, .framerates = 96},
+	{.width = 1920, .height = 1200, .framerates = 87},
 };
 
 static struct resolution ar0521_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},
-	{.width = 1920, .height = 1080, .framerates = 30},
-	{.width = 2592, .height = 1944, .framerates = 30},
+	{.width = 1280, .height = 720, .framerates = 60},
+	{.width = 1920, .height = 1080, .framerates = 60},
+	{.width = 2560, .height = 1440, .framerates = 40},
+	{.width = 2592, .height = 1944, .framerates = 40},
+};
+
+static struct resolution ar0522_res_list[] = {
+	{.width = 1280, .height = 720, .framerates = 60},
+	{.width = 1920, .height = 1080, .framerates = 60},
+	{.width = 2560, .height = 1440, .framerates = 60},
+	{.width = 2592, .height = 1944, .framerates = 40},
 };
 
 static struct resolution ar0821_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},
-	{.width = 1920, .height = 1080, .framerates = 30},
-	{.width = 2560, .height = 1440, .framerates = 30},
-	{.width = 3840, .height = 2160, .framerates = 20},
+	{.width = 1280, .height = 720, .framerates = 40},
+	{.width = 1920, .height = 1080, .framerates = 40},
+	{.width = 3840, .height = 2160, .framerates = 24},
 };
 
 static struct resolution ar1335_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},    //HD  720p
-	{.width = 1920, .height = 1080, .framerates = 30},   //FHD 1080p
-	{.width = 2560, .height = 1440, .framerates = 30},   //2K  1440p
-	{.width = 3840, .height = 2160, .framerates = 30},   //4K  2160p
-	{.width = 4192, .height = 3120, .framerates = 20},
-	{.width = 4208, .height = 3120, .framerates = 20},
+	{.width = 640, .height = 480, .framerates = 120},    //HD  480p
+	{.width = 1280, .height = 720, .framerates = 60},    //HD  720p
+	{.width = 1920, .height = 1080, .framerates = 60},   //FHD 1080p
+	// {.width = 2560, .height = 1440, .framerates = 30},   //2K  1440p
+	// {.width = 3840, .height = 2160, .framerates = 30},   //4K  2160p
+	{.width = 4192, .height = 3120, .framerates = 24},
+	{.width = 4208, .height = 3120, .framerates = 15},
 };
 
 static struct resolution ar1820_res_list[] = {
@@ -72,8 +80,8 @@ static struct sensor_info ap1302_sensor_table[] = {
 	},
 	{
 		.sensor_name = "TEVI-AR0522",
-		.res_list = ar0521_res_list,
-		.res_list_size = ARRAY_SIZE(ar0521_res_list)
+		.res_list = ar0522_res_list,
+		.res_list_size = ARRAY_SIZE(ar0522_res_list)
 	},
 	{
 		.sensor_name = "TEVI-AR0821",
