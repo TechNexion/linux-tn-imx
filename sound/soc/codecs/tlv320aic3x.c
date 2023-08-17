8d5dc865db00b4c4485f4719d211b551f2c8457c
Joe Zhang <joe.zhang@technexion.com>
2023-08-17 13:37:04 +0800

    sound: tlv320aic3x: change gpio_set_value to gpiod_set_value_cansleep
    Prevent gpiolib driver show __WARN(); when gpio controller is cansleep.

--- sound/soc/codecs/tlv320aic3x.c
+++ sound/soc/codecs/tlv320aic3x.c
@@ -1400,7 +1400,7 @@ static int aic3x_set_power(struct snd_soc_component *component, int power)
 
 		if (aic3x->gpio_reset) {
 			udelay(1);
-			gpiod_set_value(aic3x->gpio_reset, 0);
+			gpiod_set_value_cansleep(aic3x->gpio_reset, 0);
 		}
 
 		if (aic3x->model == AIC3X_MODEL_3007)
