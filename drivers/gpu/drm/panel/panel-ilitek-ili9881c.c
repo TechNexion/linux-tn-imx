90b1bd88ce346eaacca3619494fe72bae91cc6a8
Richard Hu <richard.hu@technexion.com>
2023-08-24 13:38:25 +0800

    drm/panel: ILI9881c: fix driver structure for linux 6.1

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -1311,20 +1311,7 @@ static int ili9881c_get_modes(struct drm_panel *panel,
 	connector->display_info.width_mm = 62;
 	connector->display_info.height_mm = 110;
 
-	/*
-	 * TODO: Remove once all drm drivers call
-	 * drm_connector_set_orientation_from_panel()
-	 */
-	drm_connector_set_panel_orientation(connector, ctx->orientation);
-
 	return 1;
-}
-
-static enum drm_panel_orientation ili9881c_get_orientation(struct drm_panel *panel)
-{
-	struct ili9881c *ctx = panel_to_ili9881c(panel);
-
-	return ctx->orientation;
 }
 
 static const struct drm_panel_funcs ili9881c_funcs = {
@@ -1333,7 +1320,6 @@ static const struct drm_panel_funcs ili9881c_funcs = {
 	.enable		= ili9881c_enable,
 	.disable	= ili9881c_disable,
 	.get_modes	= ili9881c_get_modes,
-	.get_orientation = ili9881c_get_orientation,
 };
 
 static int ili9881c_dsi_probe(struct mipi_dsi_device *dsi)
@@ -1364,13 +1350,6 @@ static int ili9881c_dsi_probe(struct mipi_dsi_device *dsi)
 
 	if (IS_ERR(ctx->reset)) {
 		dev_err(&dsi->dev, "Couldn't get our reset GPIO\n");
-	}
-
-	ret = of_drm_get_panel_orientation(dsi->dev.of_node, &ctx->orientation);
-	if (ret) {
-		dev_err(&dsi->dev, "%pOF: failed to get orientation: %d\n",
-			dsi->dev.of_node, ret);
-		return ret;
 	}
 
 	ctx->panel.prepare_prev_first = true;
@@ -1449,7 +1428,6 @@ static const struct ili9881c_desc am8001280g_desc = {
 static const struct of_device_id ili9881c_of_match[] = {
 	{ .compatible = "startek,kd050hdfia020", .data = &kd050hdfia020_desc },
 	{ .compatible = "bananapi,lhr050h41" },	
-	{ .compatible = "wanchanglong,w552946aba", .data = &w552946aba_desc },
 	{ .compatible = "ampire,am8001280g", .data = &am8001280g_desc },
 	{ }
 };
