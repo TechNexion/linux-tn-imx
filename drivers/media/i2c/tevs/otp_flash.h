7b4f5414588a75c15bb4af1f1199a177efbc447b
Nian Yang <nian.yang@technexion.com>
2023-09-04 22:55:27 -0700

    media: tevs: modified bootup wait status.

--- drivers/media/i2c/tevs/otp_flash.h
+++ drivers/media/i2c/tevs/otp_flash.h
@@ -135,31 +135,13 @@
 #define HOST_COMMAND_ISP_CTRL_CT_MIN                            (0x2074)
 #define HOST_COMMAND_ISP_CTRL_SYSTEM_START                      (0x2076)
 
-#define BOOT_DATA_START_REG (0x8000)
-#define BOOT_DATA_END_REG (0x9FFF)
-#define BOOT_DATA_WRITE_LEN (BOOT_DATA_END_REG - BOOT_DATA_START_REG + 1)
-
 struct otp_flash {
 	struct device *dev;
 	struct nvmem_device *nvmem;
-	char* product_name;
+	char *product_name;
 	void *header_data;
 };
 
-/*
-header Version 2 : {
-	uint8 header_version
-	uint16 content_offset //content_offset may not be equal to the size of header
-	uint8 product_name[64]
-	uint8 product_version
-	uint8 lens_name[64]
-	uint8 lens_version
-	uint8 content_version
-	uint32 content_checksum
-	uint32 content_len
-	uint16 pll_bootdata_len
-}
-*/
 struct header_ver2 {
 	u8 header_version;
 	u16 content_offset;
@@ -172,30 +154,6 @@ struct header_ver2 {
 	u32 content_len;
 	u16 pll_bootdata_len;
 } __attribute__((packed));
-
-// struct VzHeaderV3 {   // Header Version 3
-// 	// Fixed Area
-	// uint8_t header_version;
-	// uint16_t content_offset;
-	// uint16_t sensor_type;
-	// uint8_t sensor_fuseid[16];
-	// uint8_t product_name[64];
-	// uint8_t lens_id[16];
-	// uint16_t fix_checksum;
-	// uint8_t tn_fw_version[2];
-	// uint16_t vendor_fw_version;
-	// uint16_t custom_number;
-	// uint8_t build_year;
-	// uint8_t build_month;
-	// uint8_t build_day;
-	// uint8_t build_hour;
-	// uint8_t build_minute;
-	// uint8_t build_second;
-	// uint16_t mipi_datarate;
-	// uint32_t content_len;
-	// uint16_t content_checksum;
-	// uint16_t total_checksum;
-// };
 
 struct header_ver3 {
 	u8 header_version;
