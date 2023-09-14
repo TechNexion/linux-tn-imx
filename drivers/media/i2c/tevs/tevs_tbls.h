901d919272eda45b496062cb2193fa493dd6de36
Jerry Liu <jerry.liu@technexion.com>
2023-09-14 11:30:03 +0800

    media: tevs: add hw-reset-mode to decide power mode for tevs.
    If set hw-reset-mode in device-tree of tevs, it will shutdown
    camera and recovery all parameters after closing tevs caemra.
    
    media: tevs: add sensor mode for tevs camera.
    
    Add sensor mode to improve the performances for tevs.

--- drivers/media/i2c/tevs/tevs_tbls.h
+++ drivers/media/i2c/tevs/tevs_tbls.h
@@ -5,48 +5,54 @@ struct resolution {
 	u16 width;
 	u16 height;
 	u16 framerates;
+	u16 mode;
 };
 
 static struct resolution ar0144_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 60},
-	{.width = 1280, .height = 800, .framerates = 60},
+	{.width = 640, .height = 480, .framerates = 60, .mode = 0},
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 0},
+	{.width = 1280, .height = 800, .framerates = 60, .mode = 0},
 };
 
 static struct resolution ar0234_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 120},
-	{.width = 1920, .height = 1080, .framerates = 85},
-	{.width = 1920, .height = 1200, .framerates = 60},
+	{.width = 640, .height = 480, .framerates = 120, .mode = 1},
+	{.width = 1280, .height = 720, .framerates = 120, .mode = 0},
+	{.width = 1920, .height = 1080, .framerates = 85, .mode = 0},
+	{.width = 1920, .height = 1200, .framerates = 60, .mode = 0},
 };
 
 static struct resolution ar0521_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 60},
-	{.width = 1920, .height = 1080, .framerates = 60},
-	{.width = 2560, .height = 1440, .framerates = 40},
-	{.width = 2592, .height = 1944, .framerates = 40},
+	{.width = 640, .height = 480, .framerates = 60, .mode = 3},
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
+	{.width = 1280, .height = 960, .framerates = 60, .mode = 3},
+	{.width = 1920, .height = 1080, .framerates = 60, .mode = 1},
+	{.width = 2560, .height = 1440, .framerates = 30, .mode = 1},
+	{.width = 2592, .height = 1944, .framerates = 30, .mode = 1},
 };
 
 static struct resolution ar0522_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 60},
-	{.width = 1920, .height = 1080, .framerates = 60},
-	{.width = 2560, .height = 1440, .framerates = 40},
-	{.width = 2592, .height = 1944, .framerates = 40},
+	{.width = 640, .height = 480, .framerates = 60, .mode = 3},
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
+	{.width = 1920, .height = 1080, .framerates = 60, .mode = 1},
+	{.width = 2560, .height = 1440, .framerates = 40, .mode = 1},
+	{.width = 2592, .height = 1944, .framerates = 40, .mode = 1},
 };
 
 static struct resolution ar0821_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},
-	{.width = 1920, .height = 1080, .framerates = 30},
-	{.width = 2560, .height = 1440, .framerates = 30},   //2K  1440p
-	{.width = 3840, .height = 2160, .framerates = 24},
+	{.width = 640, .height = 480, .framerates = 30, .mode = 1},
+	{.width = 1280, .height = 720, .framerates = 30, .mode = 1},
+	{.width = 1920, .height = 1080, .framerates = 30, .mode = 1},
+	{.width = 2560, .height = 1440, .framerates = 30, .mode = 0},   //2K  1440p
+	{.width = 3840, .height = 2160, .framerates = 20, .mode = 0},
 };
 
 static struct resolution ar1335_res_list[] = {
-	// {.width = 640, .height = 480, .framerates = 120},    //HD  480p
-	{.width = 1280, .height = 720, .framerates = 30},    //HD  720p
-	{.width = 1920, .height = 1080, .framerates = 30},   //FHD 1080p
-	{.width = 2560, .height = 1440, .framerates = 30},   //2K  1440p
-	{.width = 3840, .height = 2160, .framerates = 24},   //4K  2160p
-	// {.width = 4192, .height = 3120, .framerates = 24},
-	// {.width = 4208, .height = 3120, .framerates = 15},
+	{.width = 640, .height = 480, .framerates = 60, .mode = 4},     //VGA  480p
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 4},    //HD  720p
+	{.width = 1920, .height = 1080, .framerates = 60, .mode = 3},   //FHD 1080p
+	{.width = 2560, .height = 1440, .framerates = 30, .mode = 1},   //2K  1440p
+	{.width = 3840, .height = 2160, .framerates = 20, .mode = 0},   //4K  2160p
+	{.width = 4208, .height = 3120, .framerates = 15, .mode = 0},   //13M
 };
 
 static struct resolution unknown_res_list[] = {
