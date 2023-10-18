a69c0bd278a4b13c32281c9da7159cd3b6c755a3
Joe Zhang <joe.zhang@technexion.com>
2023-10-18 15:14:11 +0800

    media: ds90ub94x: add mask for skip check some regs
    To check all bit in reg, set mask to 0xFF.
    To skip check BIT(2) in reg, set mask to 0xFB.
    To skip check all bit in reg, set mask to 0x00.

--- drivers/media/i2c/ds90ub94x.c
+++ drivers/media/i2c/ds90ub94x.c
@@ -6,6 +6,8 @@
 
 #define DS90UB941_I2C_ADDR 0x0c
 #define DS90UB948_I2C_ADDR 0x2c
+#define BYTE_MASK 0xFF
+#define XNOR(x, y)	(~(x ^ y) & BYTE_MASK)
 
 #define RESET_MDELAY 50
 #define I2C_RW_MDELAY 50
@@ -26,17 +28,19 @@
 	_resultb_;					\
 })
 
-#define REGMAP_RETRY_READ( ds90ub94x, reg, val)		\
+#define REGMAP_RETRY_READ( ds90ub94x, reg, val, mask)	\
 ({							\
 	int _attemptc_ = REGMAP_RETRY_COUNT;		\
-	int val_read;					\
+	int val_read, check_val;			\
 	int _resultb_;					\
 	while ( _attemptc_-- ){				\
 		_resultb_ = 0;				\
 		regmap_read(ds90ub94x->regmap, reg, &val_read);	\
 		dev_dbg(ds90ub94x->dev, "REGMAP_RETRY_READ: reg 0x%02x, value 0x%02x\n", reg, val_read); \
-		if (( val_read == val )) break;		\
-		dev_err(ds90ub94x->dev, "REGMAP_RETRY_READ: failed: reg 0x%02x, value 0x%02x, retry = %d\n", reg, val_read, (REGMAP_RETRY_COUNT - _attemptc_)); \
+		check_val = XNOR(val, val_read);	\
+		check_val &= mask;			\
+		if ( mask == check_val ) break;		\
+		dev_err(ds90ub94x->dev, "REGMAP_RETRY_READ: failed: reg 0x%02x, value 0x%02x, mask 0x%02x, retry = %d\n", reg, val, mask, (REGMAP_RETRY_COUNT - _attemptc_)); \
 		_resultb_ = -1;				\
 		msleep(I2C_RW_MDELAY);			\
 	}						\
@@ -78,66 +82,67 @@ struct ds90ub94x {
 struct i2c_config{
 	unsigned char i2c_reg;
 	unsigned char i2c_val;
+	unsigned char check_mask;
 };
 
 struct i2c_config ds90ub941_reset[] = {
-	{0x01, 0x0E},
-	{0x04, 0x20}, //clear CRC_ERROR count
+	{0x01, 0x0E, 0x00},
+	{0x04, 0x20, 0x00}, //clear CRC_ERROR count
 };
 
 struct i2c_config ds90ub948_reset[] = {
-	{0x01, 0x03},
+	{0x01, 0x03, 0x00},
 };
 
 struct i2c_config ds90ub941_dual_channel[] = {
-	{0x5B, 0x29},
+	{0x5B, 0x29, 0x08},
 };
 
 struct i2c_config ds90ub948_dual_channel[] = {
-	{0x49, 0x60},
+	{0x49, 0x60, 0x02},
 };
 
 struct i2c_config ds90ub941_probe_config[] = {
-	{0x04, 0x00}, //start CRC_ERROR count
-	{0x03, 0x9A}, //Enable FPD-Link I2C pass through, DSI clock auto switch
+	{0x04, 0x00, 0x33}, //start CRC_ERROR count
+	{0x03, 0x9A, 0xFA}, //Enable FPD-Link I2C pass through, DSI clock auto switch
 
-	{0x1E, 0x01}, //Select FPD-Link III Port 0
-	{0x5B, 0x21}, //FPD3_TX_MODE=single, Reset PLL
-	{0x4F, 0x8C}, //DSI Continuous Clock Mode,DSI 4 lanes
-	{0x0D, 0x33}, //GPIO0, MIPI_BL_EN
-	{0x0E, 0x33}, //GPIO1, MIPI_VDDEN; GPIO2, MIPI_BL_PWM
-	{0x0F, 0x03}, //Reset touch interrupt
+	{0x1E, 0x01, 0x07}, //Select FPD-Link III Port 0
+	{0x5B, 0x21, 0xFF}, //FPD3_TX_MODE=single, Reset PLL
+	{0x4F, 0x8C, 0xFE}, //DSI Continuous Clock Mode,DSI 4 lanes
+	{0x0D, 0x03, 0x0F}, //GPIO0, MIPI_BL_EN
+	{0x0E, 0x33, 0xFF}, //GPIO1, MIPI_VDDEN; GPIO2, MIPI_BL_PWM
+	{0x0F, 0x03, 0x0F}, //Reset touch interrupt
 
-	{0x01, 0x00}, //Release DSI/DIGITLE reset
+	{0x01, 0x00, 0x0F}, //Release DSI/DIGITLE reset
 
-	{0x07, 0x54}, //SlaveID_0: touch panel, 0x2A << 1
-	{0x08, 0x54}, //SlaveAlias_0: touch panel, 0x2A << 1
-	{0x70, 0xAC}, //SlaveID_1: EEPROM, 0x56 << 1
-	{0x77, 0xAC}, //SlaveAlias_1: EEPROM, 0x56 << 1
+	{0x07, 0x54, 0xFE}, //SlaveID_0: touch panel, 0x2A << 1
+	{0x08, 0x54, 0xFE}, //SlaveAlias_0: touch panel, 0x2A << 1
+	{0x70, 0xAC, 0xFE}, //SlaveID_1: EEPROM, 0x56 << 1
+	{0x77, 0xAC, 0xFE}, //SlaveAlias_1: EEPROM, 0x56 << 1
 
-	{0xC6, 0x21}, //REM_INTB the same as INTB_IN on UB948, Global Interrupt Enable 
+	{0xC6, 0x21, 0xFF}, //REM_INTB the same as INTB_IN on UB948, Global Interrupt Enable 
 };
 
 struct i2c_config ds90ub948_probe_config[] = {
-	{0x03, 0xF8}, //enable CRC, i2c pass through
-	{0x49, 0x62}, //Set FPD_TX_MODE, MAPSEL=1(SPWG), Single OLDI output
-	{0x34, 0x01}, //Select FPD-Link III Port 0
-	{0x26, 0x19}, //SCL_HIGH_TIME: 1.5 us (50 ns * 0x19)
-	{0x27, 0x19}, //SCL_LOW_TIME: 1.5 us (50 ns * 0x19)
-	{0x1D, 0x15}, //GPIO0, MIPI_BL_EN
-	{0x1E, 0x55}, //GPIO1, MIPI_VDDEN; GPIO2, MIPI_BL_PWM
-	{0x1F, 0x05}, //Reset touch interrupt
+	{0x03, 0xF8, 0xFE}, //enable CRC, i2c pass through
+	{0x49, 0x62, 0xE3}, //Set FPD_TX_MODE, MAPSEL=1(SPWG), Single OLDI output
+	{0x34, 0x01, 0xFB}, //Select FPD-Link III Port 0
+	{0x26, 0x19, 0xFF}, //SCL_HIGH_TIME: 1.5 us (50 ns * 0x19)
+	{0x27, 0x19, 0xFF}, //SCL_LOW_TIME: 1.5 us (50 ns * 0x19)
+	{0x1D, 0x15, 0x0F}, //GPIO0, MIPI_BL_EN
+	{0x1E, 0x55, 0xFF}, //GPIO1, MIPI_VDDEN; GPIO2, MIPI_BL_PWM
+	{0x1F, 0x05, 0x0F}, //Reset touch interrupt
 
-	{0x08, 0x54}, //TargetID_0: touch panel, 0x2A << 1
-	{0x10, 0x54}, //TargetALIAS_0: touch panel, 0x2A << 1
-	{0x09, 0xAC}, //TargetID_1: EEPROM, 0x56 << 1
-	{0x11, 0xAC}, //TargetALIAS_1: EEPROM, 0x56 << 1
+	{0x08, 0x54, 0xFE}, //TargetID_0: touch panel, 0x2A << 1
+	{0x10, 0x54, 0xFE}, //TargetALIAS_0: touch panel, 0x2A << 1
+	{0x09, 0xAC, 0xFE}, //TargetID_1: EEPROM, 0x56 << 1
+	{0x11, 0xAC, 0xFE}, //TargetALIAS_1: EEPROM, 0x56 << 1
 };
 
 static int regmap_i2c_rw_check_retry(struct ds90ub94x *ds90ub94x, struct i2c_config *i2c_config, int i2c_config_size)
 {
 	int i;
-	unsigned int reg, val;
+	unsigned int reg, val, mask;
 
 	//continue write
 	for ( i = 0; i < i2c_config_size ; i++ )
@@ -154,8 +159,9 @@ static int regmap_i2c_rw_check_retry(struct ds90ub94x *ds90ub94x, struct i2c_con
 	{
 		reg = i2c_config[i].i2c_reg;
 		val = i2c_config[i].i2c_val;
+		mask = i2c_config[i].check_mask;
 
-		if (REGMAP_RETRY_READ(ds90ub94x, reg, val) != 0)
+		if (REGMAP_RETRY_READ(ds90ub94x, reg, val, mask) != 0)
 			return -EIO;
 	}
 	return 0;
