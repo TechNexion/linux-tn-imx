1bc22bbb3a2a27de54a8151d41e4320695c2c022
Joe Zhang <joe.zhang@technexion.com>
2024-06-11 15:44:17 +0800

    fbdev: tweak for driver to fetch cmdline.
    This patch is tweak for lf-6.1.y, there are patches on lf-6.6.y.
    Please don't apply this patch on futher branch.
    
    93604a5ade3a fbdev: Handle video= parameter in video/cmdline.c
    367221793d47 fbdev: Move option-string lookup into helper
    6d8ad3406a69 fbdev: Unexport fb_mode_option
    cedaf7cddd73 fbdev: Support NULL for name in option-string lookup
    73ce73c30ba9 fbdev: Transfer video= option strings to caller; clarify ownership

--- drivers/video/fbdev/core/fb_cmdline.c
+++ drivers/video/fbdev/core/fb_cmdline.c
@@ -17,11 +17,86 @@
 #include <linux/init.h>
 #include <linux/fb.h>
 
-static char *video_options[FB_MAX] __read_mostly;
 static int ofonly __read_mostly;
 
 const char *fb_mode_option;
 EXPORT_SYMBOL_GPL(fb_mode_option);
+
+/*
+ * FB_MAX is the maximum number of framebuffer devices and also
+ * the maximum number of video= parameters. Although not directly
+ * related to each other, it makes sense to keep it that way.
+ */
+static const char *video_options[FB_MAX] __read_mostly;
+static const char *video_option __read_mostly;
+static int video_of_only __read_mostly;
+
+static const char *__video_get_option_string(const char *name)
+{
+	const char *options = NULL;
+	size_t name_len = 0;
+
+	if (name)
+		name_len = strlen(name);
+
+	if (name_len) {
+		unsigned int i;
+		const char *opt;
+
+		for (i = 0; i < ARRAY_SIZE(video_options); ++i) {
+			if (!video_options[i])
+				continue;
+			if (video_options[i][0] == '\0')
+				continue;
+			opt = video_options[i];
+			if (!strncmp(opt, name, name_len) && opt[name_len] == ':')
+				options = opt + name_len + 1;
+		}
+	}
+
+	/* No match, return global options */
+	if (!options)
+		options = video_option;
+
+	return options;
+}
+
+/**
+ * video_get_options - get kernel boot parameters
+ * @name:	name of the output as it would appear in the boot parameter
+ *		line (video=<name>:<options>)
+ *
+ * Looks up the video= options for the given name. Names are connector
+ * names with DRM, or driver names with fbdev. If no video option for
+ * the name has been specified, the function returns the global video=
+ * setting. A @name of NULL always returns the global video setting.
+ *
+ * Returns:
+ * The string of video options for the given name, or NULL if no video
+ * option has been specified.
+ */
+const char *video_get_options(const char *name)
+{
+	return __video_get_option_string(name);
+}
+EXPORT_SYMBOL(video_get_options);
+
+bool __video_get_options(const char *name, const char **options, bool is_of)
+{
+	bool enabled = true;
+	const char *opt = NULL;
+
+	if (video_of_only && !is_of)
+		enabled = false;
+
+	opt = __video_get_option_string(name);
+
+	if (options)
+		*options = opt;
+
+	return enabled;
+}
+EXPORT_SYMBOL(__video_get_options);
 
 /**
  * fb_get_options - get kernel boot parameters
@@ -34,35 +109,28 @@ EXPORT_SYMBOL_GPL(fb_mode_option);
  */
 int fb_get_options(const char *name, char **option)
 {
-	char *opt, *options = NULL;
-	int retval = 0;
-	int name_len = strlen(name), i;
+	const char *options = NULL;
+	bool is_of = false;
+	bool enabled;
 
-	if (name_len && ofonly && strncmp(name, "offb", 4))
-		retval = 1;
+	if (name)
+		is_of = strncmp(name, "offb", 4);
 
-	if (name_len && !retval) {
-		for (i = 0; i < FB_MAX; i++) {
-			if (video_options[i] == NULL)
-				continue;
-			if (!video_options[i][0])
-				continue;
-			opt = video_options[i];
-			if (!strncmp(name, opt, name_len) &&
-			    opt[name_len] == ':')
-				options = opt + name_len + 1;
-		}
+	enabled = __video_get_options(name, &options, is_of);
+
+	if (options) {
+		if (!strncmp(options, "off", 3))
+			enabled = false;
 	}
-	/* No match, pass global option */
-	if (!options && option && fb_mode_option)
-		options = kstrdup(fb_mode_option, GFP_KERNEL);
-	if (options && !strncmp(options, "off", 3))
-		retval = 1;
 
-	if (option)
-		*option = options;
+	if (option) {
+		if (options)
+			*option = kstrdup(options, GFP_KERNEL);
+		else
+			*option = NULL;
+	}
 
-	return retval;
+	return enabled ? 0 : 1; // 0 on success, 1 otherwise
 }
 EXPORT_SYMBOL(fb_get_options);
 
