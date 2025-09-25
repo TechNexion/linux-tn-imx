54c44a0d3830558d63dbc4d0dfb6f556183cfc27
Richard Hu <richard.hu@technexion.com>
2025-09-25 15:19:43 +0800

    PCI: imx95: disable i.MX95 PCIe core reset
    The recently added cold reset sequence for i.MX95 PCIe causes intermittent
    link failures on some platforms, preventing NVMe devices from being detected.
    
    Disabling this reset function restores reliable PCIe link-up at Gen3 speed.

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -597,6 +597,7 @@ static void imx7d_pcie_wait_for_phy_pll_lock(struct imx_pcie *imx_pcie)
 		dev_err(dev, "PCIe PLL lock timeout\n");
 }
 
+#if 0
 static int imx95_pcie_wait_for_phy_pll_lock(struct imx_pcie *imx_pcie)
 {
 	u32 val;
@@ -615,6 +616,7 @@ static int imx95_pcie_wait_for_phy_pll_lock(struct imx_pcie *imx_pcie)
 	imx_pcie->pll_locked = true;
 	return 0;
 }
+#endif
 
 static int imx_setup_phy_mpll(struct imx_pcie *imx_pcie)
 {
@@ -929,6 +931,7 @@ static int imx7d_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 	return 0;
 }
 
+#if 0
 static int imx95_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 {
 	u32 val;
@@ -967,6 +970,7 @@ static int imx95_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 
 	return 0;
 }
+#endif
 
 static void imx_pcie_assert_core_reset(struct imx_pcie *imx_pcie)
 {
@@ -2209,7 +2213,9 @@ static const struct imx_pcie_drvdata drvdata[] = {
 		.mode_off[0]  = IMX95_PE0_GEN_CTRL_1,
 		.mode_mask[0] = IMX95_PCIE_DEVICE_TYPE,
 		.init_phy = imx95_pcie_init_phy,
+		#if 0
 		.core_reset = imx95_pcie_core_reset,
+		#endif
 		.post_config = imx95_pcie_post_config,
 	},
 	[IMX6Q_EP] = {
