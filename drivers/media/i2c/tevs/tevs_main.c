601ad73621e5dac890634c589c778b1a7ee02f76
Nian Yang <nian.yang@technexion.com>
2026-04-21 17:25:24 +0800

    media: tevs: support arrays of framerates for resolutions
    Update the resolution structs across various sensor configurations in all tev cameras to support an array of framerates rather than a single fixed framerate.
    This provides a wider range of available framerates for each resolution mode.

--- drivers/media/i2c/tevs/tevs_main.c
+++ drivers/media/i2c/tevs/tevs_main.c
@@ -340,6 +340,7 @@ struct tevs {
 	int trigger_mode;
 	char *sensor_name;
 	int vc_id;
+	unsigned int fps;
 
 	/* V4L2 Controls */
 	struct v4l2_ctrl_handler ctrls;
@@ -596,19 +597,15 @@ static int tevs_get_frame_interval(struct v4l2_subdev *sub_dev,
 				   struct v4l2_subdev_frame_interval *fi)
 {
 	struct tevs *tevs = container_of(sub_dev, struct tevs, v4l2_subdev);
-	u32 max_fps;
 
 	if (fi->pad != 0)
 		return -EINVAL;
 
 	dev_dbg(sub_dev->dev, "%s()\n", __func__);
 
-	max_fps = tevs_sensor_table[tevs->selected_sensor]
-			  .res_list[tevs->selected_mode]
-			  .framerates;
-
 	fi->interval.numerator = 1;
-	fi->interval.denominator = max_fps;
+	fi->interval.denominator = tevs->fps;
+	dev_dbg(sub_dev->dev, "fps = %d\n", tevs->fps);
 
 	return 0;
 }
@@ -618,7 +615,11 @@ static int tevs_set_frame_interval(struct v4l2_subdev *sub_dev,
 				   struct v4l2_subdev_frame_interval *fi)
 {
 	struct tevs *tevs = container_of(sub_dev, struct tevs, v4l2_subdev);
-	u32 max_fps;
+	unsigned int max_fps, min_fps;
+	unsigned int fps =
+		fi->interval.numerator ?
+			fi->interval.denominator / fi->interval.numerator :
+			fi->interval.denominator;
 
 	if (fi->pad != 0)
 		return -EINVAL;
@@ -627,10 +628,26 @@ static int tevs_set_frame_interval(struct v4l2_subdev *sub_dev,
 
 	max_fps = tevs_sensor_table[tevs->selected_sensor]
 			  .res_list[tevs->selected_mode]
-			  .framerates;
+			  .framerates[0];
+	min_fps =
+		tevs_sensor_table[tevs->selected_sensor]
+			.res_list[tevs->selected_mode]
+			.framerates
+				[ARRAY_SIZE(
+					 tevs_sensor_table[tevs->selected_sensor]
+						 .res_list[tevs->selected_mode]
+						 .framerates) -
+				 1];
+
+	if (fps > max_fps)
+		fps = max_fps;
+	else if (fps < min_fps)
+		fps = min_fps;
 
 	fi->interval.numerator = 1;
-	fi->interval.denominator = max_fps;
+	fi->interval.denominator = fps;
+	tevs->fps = fps;
+	dev_dbg(sub_dev->dev, "fps = %d\n", fps);
 
 	return 0;
 }
@@ -655,9 +672,6 @@ static int tevs_set_stream(struct v4l2_subdev *sub_dev, int enable)
 			ret = tevs_standby(tevs, 0);
 
 		if (ret == 0) {
-			int fps = tevs_sensor_table[tevs->selected_sensor]
-					  .res_list[tevs->selected_mode]
-					  .framerates;
 			dev_dbg(sub_dev->dev, "%s() width=%d, height=%d\n",
 				__func__,
 				tevs_sensor_table[tevs->selected_sensor]
@@ -693,9 +707,9 @@ static int tevs_set_stream(struct v4l2_subdev *sub_dev, int enable)
 					.height);
 			tevs_i2c_write_16b(
 				tevs, HOST_COMMAND_ISP_CTRL_PREVIEW_MAX_FPS,
-				fps);
+				tevs->fps);
 			if (tevs->max_fps)
-				tevs->max_fps->cur.val = fps;
+				tevs->max_fps->cur.val = tevs->fps;
 			tevs_i2c_read(tevs, TEVS_AE_MANUAL_EXP_TIME, exp, 4);
 			tevs->exp_time->cur.val = be32_to_cpup((__be32 *)exp) &
 						  TEVS_AE_MANUAL_EXP_TIME_MASK;
@@ -894,7 +908,10 @@ static int tevs_enum_frame_interval(struct v4l2_subdev *sub_dev,
 	struct tevs *tevs = container_of(sub_dev, struct tevs, v4l2_subdev);
 	int i;
 
-	if ((fie->pad != 0) || (fie->index != 0))
+	if ((fie->pad != 0) ||
+	    (fie->index >= ARRAY_SIZE(tevs_sensor_table[tevs->selected_sensor]
+					      .res_list[fie->index]
+					      .framerates)))
 		return -EINVAL;
 
 	dev_dbg(sub_dev->dev, "%s() index [%u]\n", __func__, fie->index);
@@ -912,7 +929,7 @@ static int tevs_enum_frame_interval(struct v4l2_subdev *sub_dev,
 			fie->interval.denominator =
 				tevs_sensor_table[tevs->selected_sensor]
 					.res_list[i]
-					.framerates;
+					.framerates[fie->index];
 			break;
 		}
 	}
@@ -2285,6 +2302,8 @@ static int tevs_probe(struct i2c_client *client)
 
 	/* Initialize source pads */
 	tevs->pad.flags = MEDIA_PAD_FL_SOURCE;
+	tevs->fps =
+		tevs_sensor_table[tevs->selected_sensor].res_list[0].framerates[0];
 	ret = media_entity_pads_init(&tevs->v4l2_subdev.entity, 1, &tevs->pad);
 	if (ret) {
 		dev_err(dev, "failed to init entity pads: %d\n", ret);
